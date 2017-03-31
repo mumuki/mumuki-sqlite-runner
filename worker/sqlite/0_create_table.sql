bf448a115a313e61f1bc6d85da743120e6ab149f
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-03-31 18:55:28 -0300

    agrego dockerfile para db sqlite

---
+++ worker/sqlite/0_create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE test (
+    id integer primary key,
+    name varchar(200) NOT NULL
+);
+
+INSERT INTO test (name) values ('Testing1');
+INSERT INTO test (name) values ('Testing2');
+INSERT INTO test (name) values ('Testing3');
