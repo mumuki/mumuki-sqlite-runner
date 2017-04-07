64d096512267fdc809570f4ddb74b0ab78ac2e41
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-04-07 10:30:13 -0300

    remove old postgres docker

--- worker/0_create_table.sql
+++ worker/0_create_table.sql
@@ -1,10 +1,8 @@
-\c mumuki;
-
-CREATE TABLE public.test (
-    id bigserial primary key,
+CREATE TABLE test (
+    id integer primary key,
     name varchar(200) NOT NULL
 );
 
-INSERT INTO public.test (name) values ('Testing1');
-INSERT INTO public.test (name) values ('Testing2');
-INSERT INTO public.test (name) values ('Testing3');
+INSERT INTO test (name) values ('Testing1');
+INSERT INTO test (name) values ('Testing2');
+INSERT INTO test (name) values ('Testing3');
