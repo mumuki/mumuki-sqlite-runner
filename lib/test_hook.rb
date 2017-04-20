382b67f60f40fc390be0bd99917b929cb5c2dbe5
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-04-20 11:07:18 -0300

    rdoc updated with SqliteTestHook doc

--- lib/test_hook.rb
+++ lib/test_hook.rb
@@ -1,17 +1,24 @@
 require 'pp'
 
+##
+# This Hook allow to run Sqlite Worker from an adhoc program that receives .sql files.
+
 class SqliteTestHook < Mumukit::Templates::FileHook
 
+  # Define that worker runs on a freshly-cleaned environment
   isolated
 
+  # Just define file extension
   def tempfile_extension
     '.sql'
   end
 
+  # Define the command to be run by sqlite worker
   def command_line(filename)
     "runsql #{filename}"
   end
 
+  # Define the .sql file template from request structure
   def compile_file_content(request)
     <<~SQL
       -- CREATE
@@ -23,6 +30,7 @@ class SqliteTestHook < Mumukit::Templates::FileHook
     SQL
   end
 
+  # Define how output results
   def post_process_file(_file, result, status)
     output = result.strip
     [output, status]
