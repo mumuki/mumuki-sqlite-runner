959d7a5bd5602e396db3c989a39ebc39c967971d
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-06-14 09:22:27 -0300

    add utf-8 encoding strings cases to testmql.py

--- worker/bin/test/testmql.py
+++ worker/bin/test/testmql.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import unittest
 from mql import *
 
@@ -11,7 +14,8 @@ class TestMQL(unittest.TestCase):
         self.student = "select * from test;"
         self.datasets = [
             "insert into test (name) values ('Test 1.1');\ninsert into test (name) values ('Test 1.2');\ninsert into test (name) values ('Test 1.3');",
-            "insert into test (name) values ('Test 2.1');\ninsert into test (name) values ('Test 2.2');\ninsert into test (name) values ('Test 2.3');"
+            "insert into test (name) values ('Test 2.1');\ninsert into test (name) values ('Test 2.2');\ninsert into test (name) values ('Test 2.3');",
+            "insert into test (name) values ('Test UTF-8 áéíóúñÁÉÍÓÚÑ');",
         ]
 
     def test_run_ok(self):
@@ -29,12 +33,14 @@ class TestMQL(unittest.TestCase):
 
         # Assert
         self.assertFalse(mql.has_error())
-        self.assertEquals(2, len(result['solutions']))
-        self.assertEquals(2, len(result['results']))
+        self.assertEquals(3, len(result['solutions']))
+        self.assertEquals(3, len(result['results']))
         self.assertEquals("name\nTest 1.1\nTest 1.2\nTest 1.3", result['solutions'][0])
         self.assertEquals("name\nTest 2.1\nTest 2.2\nTest 2.3", result['solutions'][1])
+        self.assertEquals("name\nTest UTF-8 áéíóúñÁÉÍÓÚÑ", result['solutions'][2])
         self.assertEquals("id|name\n1|Test 1.1\n2|Test 1.2\n3|Test 1.3", result['results'][0])
         self.assertEquals("id|name\n1|Test 2.1\n2|Test 2.2\n3|Test 2.3", result['results'][1])
+        self.assertEquals("id|name\n1|Test UTF-8 áéíóúñÁÉÍÓÚÑ", result['results'][2])
 
     def test_run_error(self):
         # Arrange
