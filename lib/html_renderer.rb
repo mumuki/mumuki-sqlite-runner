4fb84d958e209a002c62376b098968e0468d448f
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-06-29 23:57:14 -0300

    pretty html diff view :)

--- lib/html_renderer.rb
+++ lib/html_renderer.rb
@@ -11,18 +11,50 @@ module Sqlite
 
     def render_error(result, solution, error)
       @error = error
-      @result = {
-          header: result[:dataset].header,
-          rows: result[:dataset].rows
-      }
-      @solution = {
-          header: solution[:dataset].header,
-          rows: solution[:dataset].rows
-      }
+      @result = parse_dataset(result[:dataset].header, result[:dataset].rows)
+      @solution = parse_dataset(solution[:dataset].header, solution[:dataset].rows)
+
       template_file_error.result binding
     end
 
     protected
+
+    def parse_dataset(header, rows)
+      header_sign = header.shift
+      rows = rows.map do |row|
+        {
+            sign: row.shift,
+            row: row
+        }
+      end
+
+      {
+          header: {
+              sign: header_sign,
+              class: diff_class_of(header_sign),
+              fields: header
+          },
+          rows: rows.map do |row|
+            {
+                sign:row[:sign],
+                class: diff_class_of(row[:sign]),
+                fields: row[:row]
+            }
+          end
+      }
+
+    end
+
+    def diff_class_of(value)
+      case value
+        when '+'
+          'required'
+        when '-'
+          'errored'
+        else
+          'nothing'
+      end
+    end
 
     def template_file_success
       ERB.new File.read("#{__dir__}/view/rows_success.html.erb")
