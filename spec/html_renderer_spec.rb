b5e31909eaadffa4e4389ed4b9d45880aa6321fa
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-10-21 18:16:53 -0300

    [drafts] fixed html_renderer_spec

--- spec/html_renderer_spec.rb
+++ spec/html_renderer_spec.rb
@@ -5,20 +5,31 @@ describe Sqlite::HtmlRenderer do
 
   describe '#render_success' do
     let(:render) do
-      renderer.render_success({ id:1, dataset: Sqlite::Dataset.new("id|name\n1|Name 1\n2|Name 2\n") })
+      result = {
+          id:1,
+          dataset: Sqlite::Dataset.new("id|name\n1|Name 1\n2|Name 2\n")
+      }
+      message = I18n.t 'message.success.query'
+      renderer.render_success result, message
     end
 
-    it { expect(render).to include I18n.t 'success.query' }
+    it { expect(render).to include I18n.t 'message.success.query' }
     it { expect(render).to include 'sqlite_success' }
     it { expect(render).not_to include 'sqlite_error' }
-    end
+  end
 
   describe '#render_error' do
-    let(:error) { I18n.t 'failure.columns' }
+    let(:error) { I18n.t 'message.failure.columns' }
     let(:render) do
-      result = { id:1, dataset: Sqlite::Dataset.new("id|name\n1|Name 1\n2|Name 2\n") }
-      solution = { id:1, dataset: Sqlite::Dataset.new("name\nName 1\nName 2\n") }
-      renderer.render_error(result, solution, error)
+      result = {
+          id:1,
+          dataset: Sqlite::Dataset.new("id|name\n1|Name 1\n2|Name 2\n")
+      }
+      solution = {
+          id:1,
+          dataset: Sqlite::Dataset.new("name\nName 1\nName 2\n")
+      }
+      renderer.render_error result, solution, error
     end
 
     it { expect(render).to include error }
